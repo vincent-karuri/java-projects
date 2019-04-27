07686bae83249466fc5bec7f17352876fec1aabb
Vincent Karuri <vincentmkaruri@gmail.com>
2019-04-27 15:18:41 +0300

    Config changes to fix view resolution errors

--- SimpleWebApp/src/main/java/com/example/karuri/vincent/HelloController.java
+++ SimpleWebApp/src/main/java/com/example/karuri/vincent/HelloController.java
@@ -1,9 +1,12 @@
 package com.example.karuri.vincent;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping
+@Controller
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
--- SimpleWebApp/src/main/webapp/WEB-INF/jsp/hello.jsp
+++ SimpleWebApp/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,9 +1,10 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page contentType = "text/html; charset = UTF-8" isELIgnored="false" %>
 <html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-<h2>${message}</h2>
-</body>
+   <head>
+      <title>Hello World</title>
+   </head>
+
+   <body>
+      <h2>${message}</h2>
+   </body>
 </html>
