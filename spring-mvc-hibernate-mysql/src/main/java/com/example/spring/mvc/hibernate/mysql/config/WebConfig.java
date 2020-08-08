c51d69af922229185fdf38189da403a1644f26ea
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-08 14:48:14 +0300

    Add web config

---
+++ spring-mvc-hibernate-mysql/src/main/java/com/example/spring/mvc/hibernate/mysql/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.spring.mvc.hibernate.mysql.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Vincent Karuri on 08/08/2020
+ */
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+	}
+}
