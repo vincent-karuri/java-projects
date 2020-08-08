fc13df5ce381f5707602f197c1401270303dc56f
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-08 14:28:47 +0300

    Add spring security initializer

---
+++ spring-mvc-hibernate-mysql/src/main/java/com/example/spring/mvc/hibernate/mysql/initializer/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.example.spring.mvc.hibernate.mysql.initializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Vincent Karuri on 08/08/2020
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
