9c49203b017a989bbc424d10d382ee4521c04fcd
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-08 14:23:16 +0300

    Add web security config for permissions and user details configuration

---
+++ spring-mvc-hibernate-mysql/src/main/java/com/example/spring/mvc/hibernate/mysql/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.spring.mvc.hibernate.mysql.config;
+
+import com.example.spring.mvc.hibernate.mysql.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by Vincent Karuri on 08/08/2020
+ */
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+				.and()
+				.authorizeRequests().antMatchers("/login**").permitAll()
+				.and()
+				.formLogin().loginPage("/login").loginProcessingUrl("/loginAction").permitAll()
+				.and()
+				.logout().logoutSuccessUrl("/login").permitAll()
+				.and()
+				.csrf().disable();
+	}
+}
