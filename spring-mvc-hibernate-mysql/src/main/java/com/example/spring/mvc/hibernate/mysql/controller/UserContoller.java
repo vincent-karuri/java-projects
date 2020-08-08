e61155c5848cf403995a957924174de86d3ea314
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-08 15:12:08 +0300

    Add UserContoller

---
+++ spring-mvc-hibernate-mysql/src/main/java/com/example/spring/mvc/hibernate/mysql/controller/UserContoller.java
@@ -0,0 +1,21 @@
+package com.example.spring.mvc.hibernate.mysql.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by Vincent Karuri on 08/08/2020
+ */
+
+@Controller
+public class UserContoller {
+
+	@GetMapping("/")
+	public String index(Model model, Principal principal) {
+		model.addAttribute("message", "You are logged in as " + principal.getName());
+		return "index";
+	}
+}
