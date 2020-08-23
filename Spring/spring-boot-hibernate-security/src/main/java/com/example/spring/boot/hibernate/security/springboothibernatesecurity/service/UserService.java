aa0df311f55a3f305f7fc38003ea78f2ac45992a
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-23 14:58:48 +0300

    Add UserService

---
+++ Spring/spring-boot-hibernate-security/src/main/java/com/example/spring/boot/hibernate/security/springboothibernatesecurity/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.spring.boot.hibernate.security.springboothibernatesecurity.service;
+
+import com.example.spring.boot.hibernate.security.springboothibernatesecurity.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    User findByEmail(String email);
+    User save(UserRegistrationDto userRegistrationDto);
+}
---
+++ Spring/spring-boot-hibernate-security/src/main/java/com/example/spring/boot/hibernate/security/springboothibernatesecurity/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.spring.boot.hibernate.security.springboothibernatesecurity.service;
+
+import com.example.spring.boot.hibernate.security.springboothibernatesecurity.dto.UserRegistrationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User save(UserRegistrationDto userRegistrationDto) {
+        User user = new User();
+        user.setFirstName(registration.getFirstName());
+        user.setLastName(registration.getLastName());
+        user.setEmail(registration.getEmail());
+        user.setPassword(passwordEncoder.encode(registration.getPassword()));
+        user.setRoles(Arrays.asList(new Role("ROLE_USER")));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
