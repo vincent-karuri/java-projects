bd84e22f73a90d21cf0fb7abca86b7d1a471ec8a
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-23 12:25:40 +0300

    Add skeleton app

---
+++ Spring/spring-boot-hibernate-security/src/main/java/com/example/spring/boot/hibernate/security/springboothibernatesecurity/SpringBootHibernateSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.hibernate.security.springboothibernatesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateSecurityApplication.class, args);
+	}
+
+}
---
+++ Spring/spring-boot-hibernate-security/src/test/java/com/example/spring/boot/hibernate/security/springboothibernatesecurity/SpringBootHibernateSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.hibernate.security.springboothibernatesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
